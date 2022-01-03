37e742c39fc7891d6817a72c3f8ac404b8e2b38d
David Foster <davidfoster1990@hotmail.co.uk>
2022-01-03 22:02:27 +0000

    first 4 challenges

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,18 @@
+function removePlayingClass(el) {
+    if (el.propertyName !== 'transform') return
+    el.target.classList.remove('playing')
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
+    const key = document.querySelector(`div[data-key='${e.keyCode}']`)
+    if (!audio) return
+
+    key.classList.add('playing')
+    audio.currentTime = 0
+    audio.play()
+}
+
+const keys = [...document.querySelectorAll('.key')]
+keys.forEach(key => key.addEventListener('transitionend', removePlayingClass))
+window.addEventListener('keydown', playSound)
